ad707b37c09efe50f1696faca16b9b73f2af5805
Mayank Malviya <98397307+Mayank2302@users.noreply.github.com>
2024-01-04 00:05:57 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+// Select tab content
+function selectItem(e) {
+  removeBorder();
+  removeShow();
+  // Add border to current tab
+  this.classList.add("tab-border");
+  // Grab content item from DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  // Add show class
+  tabContentItem.classList.add("show");
+}
+
+function removeBorder() {
+  tabItems.forEach(item => item.classList.remove("tab-border"));
+}
+
+function removeShow() {
+  tabContentItems.forEach(item => item.classList.remove("show"));
+}
+
+// Listen for tab click
+tabItems.forEach(item => item.addEventListener("click", selectItem));
+window.addEventListener('load',function() {
+  init();
+  window.addEventListener('scroll', function(){
+      // header ui update
+      const header = document.getElementById('header');
+      if (window.scrollY > 5) header.classList.add('black-bg')
+      else header.classList.remove('black-bg');
+  })
+})
